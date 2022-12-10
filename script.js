d997e07009fb84f326d607fcd62c9ad769722a52
”0xAureoleNode“ <aureolenode@gmail.com>
2022-12-10 14:55:27 +0800

    init commit

---
+++ script.js
@@ -0,0 +1,14 @@
+// object oriented programming = oop
+const circle = {
+  radius: 1,
+  location: {
+    x: 1,
+    y: 1,
+  },
+  isVisiable: true,
+  draw: function () {
+    console.log('draw');
+  },
+};
+
+circle.draw(); // draw method
