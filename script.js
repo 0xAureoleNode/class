ce520e9a387606597725076a526f3583ee33c41d
”0xAureoleNode“ <aureolenode@gmail.com>
2022-12-11 19:58:15 +0800

    exercise 5

--- script.js
+++ script.js
@@ -10,3 +10,12 @@ let post = {
   ],
   isLive: true,
 };
+
+function Post(title, body, author) {
+  this.title = title;
+  this.body = body;
+  this.author = author;
+  this.views = 0;
+  this.comments = [];
+  this.isLive = false;
+}
