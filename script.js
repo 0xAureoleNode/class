a79614a1f28b57692845e64df086e029680bab8d
”0xAureoleNode“ <aureolenode@gmail.com>
2022-12-11 19:48:11 +0800

    exercise 3

--- script.js
+++ script.js
@@ -1,24 +1,22 @@
-// exercise 2
-let address = {
-  street: 'a',
-  city: 'b',
-  zipCode: 'c',
-};
+// exercise 3
+let address1 = new Address('a', 'b', 'c');
+let address2 = new Address('a', 'b', 'c');
 
-// factory function
-function createAddress(street, city, zipCode) {
-  return {
-    street,
-    city,
-    zipCode,
-  };
-}
-
-function ShowClasses(street, city, zipCode) {
+// Constructor Function
+function Address(street, city, zipCode) {
   this.street = street;
   this.city = city;
   this.zipCode = zipCode;
 }
 
-const address1 = new ShowClasses('a', 'b', 'c');
-console.log(address1);
+function areEqual(address1, address2) {
+  return (
+    address1.street === address2.street &&
+    address1.city === address2.city &&
+    address1.zipCode === address2.zipCode
+  );
+}
+
+function areSame(address1, address2) {
+  return address1 === address2;
+}
