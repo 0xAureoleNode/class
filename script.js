188bd246ddf074f859d04b8c5852f9480426878f
”0xAureoleNode“ <aureolenode@gmail.com>
2022-12-11 19:39:32 +0800

    exercise 2 & factory function & constructor function

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
-// exercise 1
+// exercise 2
 let address = {
   street: 'a',
   city: 'b',
   zipCode: 'c',
 };
 
-const showAddress = (address) => {
-  for (let key in address) console.log(key, address[key]);
-};
+// factory function
+function createAddress(street, city, zipCode) {
+  return {
+    street,
+    city,
+    zipCode,
+  };
+}
 
-showAddress(address);
+function ShowClasses(street, city, zipCode) {
+  this.street = street;
+  this.city = city;
+  this.zipCode = zipCode;
+}
+
+const address1 = new ShowClasses('a', 'b', 'c');
+console.log(address1);
