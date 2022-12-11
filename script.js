866729f3f77be0ee6be03558b34ef5d2f4deeeb3
”0xAureoleNode“ <aureolenode@gmail.com>
2022-12-11 19:53:29 +0800

    exercise 4

--- script.js
+++ script.js
@@ -1,22 +1,12 @@
-// exercise 3
-let address1 = new Address('a', 'b', 'c');
-let address2 = new Address('a', 'b', 'c');
-
-// Constructor Function
-function Address(street, city, zipCode) {
-  this.street = street;
-  this.city = city;
-  this.zipCode = zipCode;
-}
-
-function areEqual(address1, address2) {
-  return (
-    address1.street === address2.street &&
-    address1.city === address2.city &&
-    address1.zipCode === address2.zipCode
-  );
-}
-
-function areSame(address1, address2) {
-  return address1 === address2;
-}
+// exercise 4
+let post = {
+  title: 'China is unlock down',
+  body: 'dont comment',
+  author: 'aureole han',
+  views: 656,
+  comments: [
+    { author: 'a', body: 'b' },
+    { author: 'c', body: 'd' },
+  ],
+  isLive: true,
+};
