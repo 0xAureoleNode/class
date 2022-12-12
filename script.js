80fc84316fbbf9a59fabb2dca0624e5cfb0da0f0
”0xAureoleNode“ <aureolenode@gmail.com>
2022-12-12 15:11:23 +0800

    setter & getter

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
-function interest(principal, rate = 3.5, years = 5) {
-  return ((principal * rate) / 100) * years;
-}
+const person = {
+  firstName: 'Han',
+  lastName: 'Aureole',
+  get fullName() {
+    return `${person.firstName} ${person.lastName}`;
+  },
+  set fullName(value) {
+    const parts = value.split(' ');
+    this.firstName = parts[0];
+    this.lastName = parts[1];
+  },
+};
 
-console.log(interest(100));
+person.fullName = 'John Smith';
+
+console.log(person);
