caf07e08c31a023ebdb8c2358770d9f8795df042
”0xAureoleNode“ <aureolenode@gmail.com>
2022-12-12 16:41:17 +0800

    this

--- script.js
+++ script.js
@@ -1,10 +1,31 @@
-function start() {
-  for (var i = 0; i < 5; i++) {
-    if (true) {
-      let color = 'red';
-    }
-  }
-  console.log(color);
-}
+// this
+// method => obj
+// function => global (window, global)
+// constructor function => new empty object
 
-start();
+// const video = {
+//   title: 'a',
+//   play() {
+//     console.log(this);
+//   },
+// };
+
+// const test = function () {
+//   console.log(this);
+// };
+
+// video.play();
+// test();
+
+// special function
+const video = {
+  title: 'a',
+  tags: ['a', 'b', 'c'],
+  showTags() {
+    this.tags.forEach(function (tag) {
+      console.log(this, tag);
+    });
+  },
+};
+
+video.showTags();
