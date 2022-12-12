6caa0b1c0d1e623f056768cae98c3cf680c17e54
”0xAureoleNode“ <aureolenode@gmail.com>
2022-12-12 14:56:53 +0800

    rest parameter

--- script.js
+++ script.js
@@ -1,21 +1,6 @@
-// exercise 4
-let post = {
-  title: 'China is unlock down',
-  body: 'dont comment',
-  author: 'aureole han',
-  views: 656,
-  comments: [
-    { author: 'a', body: 'b' },
-    { author: 'c', body: 'd' },
-  ],
-  isLive: true,
-};
-
-function Post(title, body, author) {
-  this.title = title;
-  this.body = body;
-  this.author = author;
-  this.views = 0;
-  this.comments = [];
-  this.isLive = false;
+function sum(discount, ...prices) {
+  const total = prices.reduce((a, b) => a + b);
+  return total * (1 - discount);
 }
+
+console.log(sum(0.1, 20, 30));
