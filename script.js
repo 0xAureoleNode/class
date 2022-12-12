066d5f2af3388a58a6a9f4ffb1bd93e4ec6ace6b
”0xAureoleNode“ <aureolenode@gmail.com>
2022-12-12 15:24:26 +0800

    try catch

--- script.js
+++ script.js
@@ -5,12 +5,17 @@ const person = {
     return `${person.firstName} ${person.lastName}`;
   },
   set fullName(value) {
+    if (typeof value !== 'string') throw new Error('Value is not a string.');
     const parts = value.split(' ');
+    if (parts.length < 2) throw new Error('Need input correct value');
     this.firstName = parts[0];
     this.lastName = parts[1];
   },
 };
-
-person.fullName = 'John Smith';
+try {
+  person.fullName = '';
+} catch (e) {
+  alert(e);
+}
 
 console.log(person);
