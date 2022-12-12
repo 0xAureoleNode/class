61fa8a7a5906123d19b1de02d0a10c14ad04e9e2
”0xAureoleNode“ <aureolenode@gmail.com>
2022-12-12 15:38:33 +0800

    scope

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
-// 局部变量优先于全局变量
-// let , const , var各不相同
+function start() {
+  for (var i = 0; i < 5; i++) {
+    if (true) {
+      let color = 'red';
+    }
+  }
+  console.log(color);
+}
+
+start();
