2202ea44dd9d1e3942673c8f9bef0b0be8fa66db
”0xAureoleNode“ <aureolenode@gmail.com>
2022-12-14 19:13:08 +0800

    this added func

--- script.js
+++ script.js
@@ -18,14 +18,37 @@
 // test();
 
 // special function
+/*
 const video = {
   title: 'a',
   tags: ['a', 'b', 'c'],
   showTags() {
+    const self = this;
     this.tags.forEach(function (tag) {
-      console.log(this, tag);
+      console.log(self.title, tag);
     });
   },
 };
 
 video.showTags();
+*/
+// function playVideo() {
+//   console.log(this);
+// }
+
+// playVideo.call({ name: 'Mosh' });
+// playVideo.apply({ name: 'Mosh' });
+
+function greet(greeting) {
+  return greeting + ' ' + this.name;
+}
+
+var person1 = {
+  name: 'Alice',
+};
+var person2 = {
+  name: 'Bob',
+};
+
+console.log(greet.call(person1, 'Hello')); // returns 'Hello Alice'
+greet.call(person2, 'Hello'); // returns 'Hello Bob'
